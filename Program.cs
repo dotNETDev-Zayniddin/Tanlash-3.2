4346211c5fe6469f75ef0b56393f44579776fd79
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-14 17:42:33 +0500

    Tanlash: Task Submission

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+    Temperatura o'girish: Foydalanuvchidan o'zbek tilida graduslar (Selsiyus) bo'yicha temperaturani kiritishni so'rang. 
+    Keyin kiritilgan temperaturani Fahrenheit va Kelvin ga o'girib, natijalarni ekranga chiqaring. 
+    Switch-operatoridan foydalaning.
+    Input: 25
+    Output: Selsiyus: 25, Fahrenheit: 77.0, Kelvin: 298.15
+*/
+using System;
+namespace Tanlash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Graduslar(°C) bo'yicha temperaturani kiriting: ");
+            int temperature = Convert.ToInt32(Console.ReadLine());  
+            switch(temperature)
+            {
+                default:
+                {
+                    decimal temperatureAsF =(decimal)((temperature * 9.0 / 5.0) + 32);
+                    decimal temperatureAsK = (decimal) (temperature + 273.15);
+                    System.Console.WriteLine($"Graduslar(°C): {temperature}\nFahrenheit(F): {temperatureAsF}\nKelvin: {temperatureAsK}");
+                    break;  
+                }
+            }
+        }
+    }
+}
